41724fe00fe0ba99f3c0241311c23922e744a33e
yuan <836444329@qq.com>
2019-03-25 15:23:59 +0800

    控制器

--- koa_react/controller/c-member.js
+++ koa_react/controller/c-member.js
@@ -27,13 +27,19 @@ exports.login = async ctx => {
             res.code = 100;
             res.msg = '没有该用户';
         }else{
-            if ( password === result[0]['password']) {
-                ctx.session = result[0];
-                res.code = 200;
-                res.msg = '登录成功';
-                res.data = result[0];
-                await userModel.updateLoginTime( moment().format('YYYY-MM-DD HH:mm:ss'), result[0].id ).then(result =>{ 
-                })
+            if ( password === result[0]['password'] ) {
+                if(!result[0]['status']){
+                    res.code = 100;
+                    res.msg = '该账号已被禁止登录';
+                }else{
+                    ctx.session = result[0];
+                    res.code = 200;
+                    res.msg = '登录成功';
+                    res.data = result[0];
+                    await userModel.updateLoginTime( moment().format('YYYY-MM-DD HH:mm:ss'), result[0].id ).then(result =>{ 
+                    })
+                }
+               
             } else {
                 res.code = 100;
                 res.msg = '用户名密码错误';
@@ -48,18 +54,32 @@ exports.login = async ctx => {
     })
 }
 
+
+
+/**
+ *获取该用户下的菜单栏
+ *   */
 exports.getMenuListByrole = async ctx => {
     ctx.body = ctx.session;
+    if(!ctx.session.id){
+        return ctx.body = {
+            code : 401,
+            msg : '请先登录'
+        }
+    }
+    console.log(ctx.session)
     await userModel.getMenuListByrole(ctx.session.roleId).then( result => {
         ctx.body = {
             code: 200,
             msg: '请求成功',
             data:result
         };
-
+    }).catch(err => {
+        ctx.body = {
+            code: 500,
+            msg: err.message
+        };
     })
-    console.log(ctx.session,111);
-
 }
 
 
